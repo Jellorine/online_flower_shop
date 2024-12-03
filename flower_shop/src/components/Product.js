e629ba7c5db682d02182097410ff238c86c5a75b
Jellorine <110653373+Jellorine@users.noreply.github.com>
2024-12-03 17:11:29 +0000

    reading the quantity from input

--- flower_shop/src/components/Product.js
+++ flower_shop/src/components/Product.js
@@ -1,5 +1,8 @@
 import '../assets/CSS/layout.css';
+import {useState} from 'react';
 export default function Product(props){
+
+    const [qty,setQty] = useState(0);
     
     return(
         <div className="grid-item">
@@ -10,7 +13,7 @@ export default function Product(props){
                     <h5 class="card-title">Price:{props.pd.price}</h5>
                     <div class="quantity-container">
                         <label for="quantity">Quantity:</label>
-                        <input type="number" id="quantity" name="quantity" />
+                        <input type="number" id="quantity" name="quantity"  value={qty} onChange={(e)=>{setQty(e.target.value)}}/>
                     </div>
                     <button class="card-button">Add to Cart</button>
                 </div>
